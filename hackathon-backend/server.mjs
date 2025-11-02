6b549e56a56fc899c613378971c30ab5332e861c
danielharton <148715880+danielharton@users.noreply.github.com>
2025-11-02 14:59:05 +0200

    Delete hackathon-backend directory

--- hackathon-backend/server.mjs
+++
@@ -1,9 +0,0 @@
-// server.mjs (handles starting the server)
-import app from './index.mjs';
-
-const port = process.env.PORT || 8080;
-
-// Start the server
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
